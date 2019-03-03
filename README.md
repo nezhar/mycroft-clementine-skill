6260824152913bed070a5a6edf10a62dcfd86770
Harald Nezbeda <hn@nezhar.com>
2019-03-03 19:48:30 +0100

    Fixes file indentation, update readme

--- README.md
+++ README.md
@@ -1,33 +1,19 @@
 # mycroft-clementine-player-plasma-skill
-This skill integrates clementine Music Player with Mycroft which enables users to Play Local Music.
 
-It is forked by the mycroft-amarok-player-plasma-skill and works in a similiar way.
-
-It could be that the plugin make conflicts with the amarok-one cause of same commands, so you propaply have to deceide what skill you want to need.
-
-The clementine-project is also for exploring the mechanisms and libs of mycroft personaly, so i also want to extend it. Ideas are welcome.
+This skill integrates [Clementine Music Player](https://www.clementine-player.org/) with [Mycroft](https://mycroft.ai/) which enables users to Play Local Music.
 
 #### Installation of skill:
+
 * Download or Clone Git
-* Create /opt/mycroft/skills folder if it does not exist
-* Extract Downloaded Skill into a folder. "mycroft-clementine-player-plasma-skill". (Clone does not require this step)
-* Copy the mycroft-internals-plasma-skill folder to /opt/mycroft/skills/ folder
+* Rename the folder to "mycroft-clementine-player-plasma-skill"
+* Move it to /opt/mycroft/skills folder
 
 #### Installation of requirements:
-##### Fedora: 
-- sudo dnf install dbus-python
-- sudo dnf install python-psutil
-- From terminal: cp -R /usr/lib64/python2.7/site-packages/dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-- From terminal: cp /usr/lib64/python2.7/site-packages/_dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-
-##### Kubuntu / KDE Neon: 
-- sudo apt install python-psutil
-- sudo apt install python-dbus
-- From terminal: cp -R /usr/lib/python2.7/dist-packages/dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-- From terminal: cp /usr/lib/python2.7/dist-packages/_dbus* /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/
-
-##### For other distributions:
-- Python Dbus and Python Psutil package is required and copying the Python Dbus folder and lib from your system python install over to /home/$USER/.virtualenvs/mycroft/lib/python2.7/site-packages/.
+##### Linux: 
+* inside the mycroft-core directory find .venv folder
+* enable venv (source .venv/bin/activate)
+* install Dbus: `pip install dbus-python`
+* install Psutil: `pip install psutil`
 
 ##### How To Use: 
 ###### Play Music/Song
@@ -65,9 +51,3 @@ Working features:
 * Stop Music
 * Next Song
 * Previous Song
-
-Known issues:
-* None
-
-TODO:
-* None
--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 import sys
 import dbus
-import glib
 import os
 import psutil
 from traceback import print_exc
@@ -10,7 +9,7 @@ from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 import subprocess
 
-__author__ = 'hersche'
+__author__ = 'nezhar'
 
 
 LOGGER = getLogger(__name__)
@@ -20,15 +19,15 @@ class ClementineMusicPlayerSkill(MycroftSkill):
     # The constructor of the skill, which calls MycroftSkill's constructor
     def __init__(self):
         super(ClementineMusicPlayerSkill, self).__init__(name="ClementineMusicPlayerSkill")
-        
+
     # This method loads the files needed for the skill's functioning, and
     # creates and registers each intent that the skill uses
     def initialize(self):
         self.load_data_files(dirname(__file__))
-        
+
         # set the downer variable to your music-player is maybe enough for make it working
         self.playerName="clementine"
-        
+
         internals_clementine_play_skill_intent = IntentBuilder("ClementinePlayKeywordIntent").\
             require("ClementinePlayKeyword").build()
         self.register_intent(internals_clementine_play_skill_intent, self.handle_internals_clementine_play_skill_intent)
@@ -40,11 +39,11 @@ class ClementineMusicPlayerSkill(MycroftSkill):
         internals_clementine_jump_backward_skill_intent = IntentBuilder("ClementineJumpBackwardKeywordIntent").\
             require("ClementineJumpKeyword").require("nrOfSongs").require("ClementineJumpBackwardKeyword").build()
         self.register_intent(internals_clementine_jump_backward_skill_intent, self.handle_internals_clementine_jumpBackward_skill_intent)
-                
+
         internals_clementine_stop_skill_intent = IntentBuilder("ClementineStopKeywordIntent").\
             require("ClementineStopKeyword").build()
         self.register_intent(internals_clementine_stop_skill_intent, self.handle_internals_clementine_stop_skill_intent)
-        
+
         internals_clementine_next_skill_intent = IntentBuilder("ClementineNextKeywordIntent").\
             require("ClementineNextKeyword").build()
         self.register_intent(internals_clementine_next_skill_intent, self.handle_internals_clementine_next_skill_intent)
@@ -52,40 +51,39 @@ class ClementineMusicPlayerSkill(MycroftSkill):
         internals_clementine_previous_skill_intent = IntentBuilder("ClementinePreviousKeywordIntent").\
             require("ClementinePreviousKeyword").build()
         self.register_intent(internals_clementine_previous_skill_intent, self.handle_internals_clementine_previous_skill_intent)
-        
+
         internals_clementine_pause_skill_intent = IntentBuilder("clementinePauseKeywordIntent").\
             require("clementinePauseKeyword").build()
         self.register_intent(internals_clementine_pause_skill_intent, self.handle_internals_clementine_pause_skill_intent)
 
 
-    def handle_internals_clementine_play_skill_intent(self, message):    
-	
-        clementineRunning = False   
+    def handle_internals_clementine_play_skill_intent(self, message):
+    
+        clementineRunning = False
 
         for proc in psutil.process_iter():
-            pinfo = proc.as_dict(attrs=['pid', 'name'])    
+            pinfo = proc.as_dict(attrs=['pid', 'name'])
             if pinfo['name'] == self.playerName:
                 clementineRunning = True
     
         if clementineRunning:
             self.speak_dialog("clementine.play")
-            #print('yes')
 
-	    def runplay():
-       		 bus = dbus.SessionBus()
-        	 remote_object = bus.get_object("org.mpris.MediaPlayer2."+self.playerName,"/org/mpris/MediaPlayer2")
-        	 remote_object.Play(dbus_interface = "org.mpris.MediaPlayer2.Player")	
-	    runplay()
+            def runplay():
+                bus = dbus.SessionBus()
+                remote_object = bus.get_object("org.mpris.MediaPlayer2."+self.playerName,"/org/mpris/MediaPlayer2")
+                remote_object.Play(dbus_interface = "org.mpris.MediaPlayer2.Player")
+            runplay()
         
-	else:
-       	   def runprocandplay():
-           	#cmdstring = "clementine %s %s %s" % ('-p' '-k' '0')
-           	#os.system(cmdstring)
-           	subprocess.call([self.playerName])
-           	bus = dbus.SessionBus()
-           	remote_object = bus.get_object("org.mpris.MediaPlayer2."+self.playerName,"/org/mpris/MediaPlayer2")
-           	remote_object.Play(dbus_interface = "org.mpris.MediaPlayer2.Player")
-           runprocandplay()
+        else:
+            def runprocandplay():
+                #cmdstring = "clementine %s %s %s" % ('-p' '-k' '0')
+                #os.system(cmdstring)
+                subprocess.call([self.playerName])
+                bus = dbus.SessionBus()
+                remote_object = bus.get_object("org.mpris.MediaPlayer2."+self.playerName,"/org/mpris/MediaPlayer2")
+                remote_object.Play(dbus_interface = "org.mpris.MediaPlayer2.Player")
+            runprocandplay()
    
     def handle_internals_clementine_stop_skill_intent(self, message):        
         bus = dbus.SessionBus()
@@ -108,7 +106,7 @@ class ClementineMusicPlayerSkill(MycroftSkill):
         nrOfSongs = message.data.get("nrOfSongs")
         try:
             parsedNrOfSongs = int(nrOfSongs)
-            i = 0;
+            i = 0
             while(parsedNrOfSongs>i):
                 if(properties_manager.Get('org.mpris.MediaPlayer2.Player', 'CanGoPrevious')):
                     remote_object.Next(dbus_interface = "org.mpris.MediaPlayer2.Player")
@@ -124,7 +122,7 @@ class ClementineMusicPlayerSkill(MycroftSkill):
         nrOfSongs = message.data.get("nrOfSongs")
         try:
             parsedNrOfSongs = int(nrOfSongs)
-            i = 0;
+            i = 0
             while(parsedNrOfSongs>i):
                 if(properties_manager.Get('org.mpris.MediaPlayer2.Player', 'CanGoPrevious')):
                     remote_object.Previous(dbus_interface = "org.mpris.MediaPlayer2.Player")
@@ -133,21 +131,20 @@ class ClementineMusicPlayerSkill(MycroftSkill):
         except ValueError:
             self.speak("sorry, was not able to parse the amount of songs i should go backward. Your wrong nr was: "+nrOfSongs)
 
-        
     def handle_internals_clementine_previous_skill_intent(self, message):
         bus = dbus.SessionBus()
         remote_object = bus.get_object("org.mpris.MediaPlayer2."+self.playerName,"/org/mpris/MediaPlayer2") 
         remote_object.Previous(dbus_interface = "org.mpris.MediaPlayer2.Player")
         
-        self.speak_dialog("clementine.previous")     
+        self.speak_dialog("clementine.previous")
 
     def handle_internals_clementine_pause_skill_intent(self, message):
         
         bus = dbus.SessionBus()
-        remote_object = bus.get_object("org.mpris.MediaPlayer2."+self.playerName,"/org/mpris/MediaPlayer2") 
+        remote_object = bus.get_object("org.mpris.MediaPlayer2."+self.playerName,"/org/mpris/MediaPlayer2")
         remote_object.Pause(dbus_interface = "org.mpris.MediaPlayer2.Player")
         
-        self.speak_dialog("clementine.pause")     
+        self.speak_dialog("clementine.pause")
         
     def stop(self):
         pass
